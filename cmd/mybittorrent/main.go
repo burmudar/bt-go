6972f31f08301a3cf0c539386b70a51b917025e5
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2023-10-20 22:29:17 +0200

    refactor

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -10,6 +10,55 @@ import (
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
+func decodeList(encoded string) (interface{}, int, error) {
+	values := make([]interface{}, 0)
+	var cursor string = encoded
+	start := 1
+	for cursor[start] != 'e' && len(cursor[start:]) != 1 {
+		value, n, err := decodeBencode(cursor[start:])
+		if err != nil {
+			return "", 0, err
+		}
+		values = append(values, value)
+		start += n + 1
+	}
+
+	return values, len(encoded) - 2, nil
+}
+
+func decodeString(encoded string) (interface{}, int, error) {
+	colonIdx := strings.IndexRune(encoded, ':')
+	if colonIdx < 0 {
+		return "", 0, fmt.Errorf("invalid string encoding: %v", encoded)
+	}
+
+	encodedLen := encoded[:colonIdx]
+
+	length, err := strconv.Atoi(encodedLen)
+	if err != nil {
+		return "", 0, err
+	}
+
+	strLen := len(encoded) - colonIdx - 1
+	if length > strLen {
+		return "", 0, fmt.Errorf("invalid length encoded - got %d but string is %d", length, strLen)
+	}
+
+	return encoded[colonIdx+1 : colonIdx+1+length], 1 + length, nil
+}
+
+func decodeInt(encoded string) (interface{}, int, error) {
+	end := strings.IndexRune(encoded, 'e')
+	if end < 0 {
+		return "", 0, fmt.Errorf("invalid integer encoding: %v", encoded)
+	}
+	num, err := strconv.Atoi(encoded[1:end])
+	if err != nil {
+		return "", 0, fmt.Errorf("failed to decode integer: %v", encoded)
+	}
+	return num, end, nil
+}
+
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
@@ -17,52 +66,15 @@ func decodeBencode(bencodedString string) (interface{}, int, error) {
 	switch {
 	case unicode.IsLetter(rune(bencodedString[0])) && bencodedString[0] == 'l':
 		{
-			values := make([]interface{}, 0)
-			var cursor string = bencodedString
-			start := 1
-			for cursor[start] != 'e' && len(cursor[start:]) != 1 {
-				value, n, err := decodeBencode(cursor[start:])
-				if err != nil {
-					return "", 0, err
-				}
-				values = append(values, value)
-				start += n + 1
-			}
-
-			return values, len(bencodedString) - 2, nil
+			return decodeList(bencodedString)
 		}
 	case unicode.IsDigit(rune(bencodedString[0])):
 		{
-			colonIdx := strings.IndexRune(bencodedString, ':')
-			if colonIdx < 0 {
-				return "", 0, fmt.Errorf("invalid string encoding: %v", bencodedString)
-			}
-
-			encodedLen := bencodedString[:colonIdx]
-
-			length, err := strconv.Atoi(encodedLen)
-			if err != nil {
-				return "", 0, err
-			}
-
-			strLen := len(bencodedString) - colonIdx - 1
-			if length > strLen {
-				return "", 0, fmt.Errorf("invalid length encoded - got %d but string is %d", length, strLen)
-			}
-
-			return bencodedString[colonIdx+1 : colonIdx+1+length], 1 + length, nil
+			return decodeString(bencodedString)
 		}
 	case unicode.IsLetter(rune(bencodedString[0])) && bencodedString[0] == 'i':
 		{
-			end := strings.IndexRune(bencodedString, 'e')
-			if end < 0 {
-				return "", 0, fmt.Errorf("invalid integer encoding: %v", bencodedString)
-			}
-			num, err := strconv.Atoi(bencodedString[1:end])
-			if err != nil {
-				return "", 0, fmt.Errorf("failed to decode integer: %v", bencodedString)
-			}
-			return num, end, nil
+			return decodeInt(bencodedString)
 		}
 	default:
 		{
