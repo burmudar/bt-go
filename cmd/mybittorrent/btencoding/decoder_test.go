95f741e6e8887da694e12b6d0dd67eea3a459b0b
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2023-10-28 12:32:59 +0200

    expected list tests

--- cmd/mybittorrent/btencoding/decoder_test.go
+++ cmd/mybittorrent/btencoding/decoder_test.go
@@ -1,37 +1,25 @@
 package btencoding
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 )
 
 func TestDecodeList(t *testing.T) {
 	tt := []struct {
-		name   string
-		value  string
-		assert func(interface{}) (bool, error)
+		name     string
+		value    string
+		expected []interface{}
 	}{
 		{
 			"empty list - le",
 			"le",
-			func(v interface{}) (bool, error) {
-				ll, ok := v.([]interface{})
-				if !ok {
-					return false, fmt.Errorf("invalid type - wanted list got %T", ll)
-				}
-				return len(ll) == 0, nil
-			},
+			[]interface{}{},
 		},
 		{
 			"le with 3 elements",
 			"l5:item15:item2i3ee",
-			func(v interface{}) (bool, error) {
-				ll, ok := v.([]interface{})
-				if !ok {
-					return false, fmt.Errorf("invalid type - wanted list got %T", ll)
-				}
-				return len(ll) == 3, nil
-			},
+			[]interface{}{"item1", "item2", 3},
 		},
 	}
 
@@ -43,15 +31,84 @@ func TestDecodeList(t *testing.T) {
 				t.Fatalf("unexpected error decoding list: %v", err)
 			}
 
-			if ok, err := tc.assert(value); err != nil {
-				t.Fatalf("assertion error: %v", err)
-			} else if !ok {
-				t.Fatalf("assertion failed - invalid type: %v", value)
+			list, ok := value.([]interface{})
+			if !ok {
+				t.Fatal("failed to cast value to list")
+			}
+			if len(list) != len(tc.expected) {
+				t.Errorf("expected list of length %d got %d", len(list), len(tc.expected))
+			}
+			for i := range tc.expected {
+				a := list[i]
+				b := tc.expected[i]
+
+				if reflect.TypeOf(a) != reflect.TypeOf(b) {
+					t.Errorf("types differ: expected %T, got %T", a, b)
+				} else if a != b {
+					t.Errorf("incorrect values: expected %v got %v", a, b)
+				}
 			}
 		})
 	}
 }
 
-func TestDecodeDict(t *testing.T) {}
+func TestDecodeNestedLists(t *testing.T) {
+	tt := []struct {
+		name         string
+		value        string
+		nesting      int
+		lastExpected []int
+	}{
+		{
+			"nesting 2 levels deep",
+			"llee",
+			1,
+			[]int{},
+		},
+		{
+			"nesting 3 levels deep",
+			"lllli1ei2ei3eeeee",
+			3,
+			[]int{1, 2, 3},
+		},
+	}
+
+	for _, tc := range tt {
+		r := NewBencodeReader(tc.value)
+		value, err := DecodeList(r)
+		if err != nil {
+			t.Fatalf("failed to decode list %q: %v", tc.value, err)
+		}
+
+		list, ok := value.([]interface{})
+		if !ok {
+			t.Fatalf("failed to cast decoded value to list: %v", value)
+		}
+
+		for tc.nesting > 0 {
+			list = list[0].([]interface{})
+			tc.nesting--
+		}
+
+		if len(list) != len(tc.lastExpected) {
+			t.Errorf("expected last nested list to have %d elements but got %d", len(tc.lastExpected), len(list))
+		}
+
+		for i, expected := range tc.lastExpected {
+			a, ok := list[i].(int)
+			if !ok {
+				t.Errorf("expected item at index %d to be int from list", i)
+			}
+
+			if a != expected {
+				t.Errorf("invalid value - expected %d got %d", tc.lastExpected[i], a)
+			}
+
+		}
+	}
+}
+
+func TestDecodeDict(t *testing.T) {
+}
 
 func TestDecodeBencode(t *testing.T) {}
