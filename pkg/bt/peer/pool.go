7b5dc3dc422902f520d396635a44bf764e6510eb
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2024-03-09 11:24:27 +0200

    wip

--- pkg/bt/peer/pool.go
+++ pkg/bt/peer/pool.go
@@ -8,9 +8,11 @@ import (
 type Pool struct {
 	peerID string
 	peers  *types.PeerSpec
+	puddle.Pool
 }
 
 func NewPool(peerID string, peers *types.PeerSpec) *Pool {
+	constructor :=
 	return &Pool{
 		peerID: peerID,
 		peers:  peers,
