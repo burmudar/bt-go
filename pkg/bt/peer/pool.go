ab1444abc99868ad1c02319877ab1d88118c74d5
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2024-03-10 16:27:00 +0200

    wrapped pool

--- pkg/bt/peer/pool.go
+++ pkg/bt/peer/pool.go
@@ -8,29 +8,52 @@ import (
 	"github.com/jackc/puddle"
 )
 
-type Pool struct {
+type peerPool struct {
 	peerID string
 	peers  *types.PeerSpec
-	puddle.Pool
+	pool   *puddle.Pool
 }
 
-func NewPool(peerID string, peers *types.PeerSpec) *Pool {
+type Pool interface {
+	Get(ctx context.Context) (*Client, func(), error)
+}
+
+func NewPool(peerID string, peers *types.PeerSpec, torrent *types.Torrent) (Pool, error) {
 	peerQueue := types.NewSyncQueue[*types.Peer]()
 	peerQueue.AddAll(peers.Peers...)
 
-	var ctor puddle.Constructor = func(ctx context.Context) (interface{}, error) {
-		_, ok := peerQueue.Pop()
+	var ctor puddle.Constructor = func(ctx context.Context) (any, error) {
+		peer, ok := peerQueue.Pop()
 		if !ok {
 			return nil, fmt.Errorf("not peers left to construct")
 		}
 
-		// TODO: user peer
-
-		return nil, nil
+		return NewHandshakedClient(peerID, peer, torrent)
 	}
 
-	return &Pool{
+	var dtor puddle.Destructor = func(res interface{}) {
+		if client, ok := res.(*Client); ok {
+			client.Close()
+		}
+	}
+
+	return &peerPool{
 		peerID: peerID,
 		peers:  peers,
+		pool:   puddle.NewPool(ctor, dtor, int32(len(peers.Peers))),
+	}, nil
+}
+
+func (p *peerPool) Get(ctx context.Context) (*Client, func(), error) {
+	res, err := p.pool.Acquire(ctx)
+	if err != nil {
+		return nil, nil, err
 	}
+
+	client, ok := res.Value().(*Client)
+	if !ok {
+		return nil, nil, fmt.Errorf("expected *peer.Client but got %T", res.Value())
+	}
+
+	return client, res.Release, nil
 }
